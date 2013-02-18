27a3138881b809151a63236475532c2547e9a0b5
Berke Viktor <bviktor@hexchat.org>
2013-02-18 03:42:35 +0100

    Make HarfBuzz a DLL

--- harfbuzz/release-x86.bat
+++ harfbuzz/release-x86.bat
@@ -10,13 +10,15 @@ pause
 
 rmdir /q /s %HARFBUZZ_DEST%
 mkdir %HARFBUZZ_DEST%
+mkdir %HARFBUZZ_DEST%\bin
 mkdir %HARFBUZZ_DEST%\include
 mkdir %HARFBUZZ_DEST%\lib
 mkdir %HARFBUZZ_DEST%\share
 mkdir %HARFBUZZ_DEST%\share\doc
 mkdir %HARFBUZZ_DEST%\share\doc\harfbuzz
+copy win32\libs\Release\harfbuzz.dll %HARFBUZZ_DEST%\bin
 copy src\*.h %HARFBUZZ_DEST%\include
-copy win32\libs\Release\harfbuzz.lib %HARFBUZZ_DEST%\lib
+copy win32\libs\harfbuzz\Release\harfbuzz.lib %HARFBUZZ_DEST%\lib
 copy COPYING %HARFBUZZ_DEST%\share\doc\harfbuzz
 
 cd %HARFBUZZ_DEST%
