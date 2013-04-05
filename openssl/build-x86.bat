6d767e96b437fd2bb81215f899cfd14d9a8f9bca
Berke Viktor <bviktor@hexchat.org>
2013-04-05 05:00:39 +0200

    PDBs for openssl and use Perl 5.18

--- openssl/build-x86.bat
+++ openssl/build-x86.bat
@@ -5,7 +5,7 @@ SET PACKAGE_NAME=openssl-1.0.1e
 
 set OPENSSL_SRC=%cd%
 set OPENSSL_DEST=%cd%-x86
-set PERL_PATH=c:\mozilla-build\perl-5.16\Win32\bin
+set PERL_PATH=c:\mozilla-build\perl-5.18\Win32\bin
 set NASM_PATH=c:\mozilla-build\nasm
 set INCLUDE=%INCLUDE%;%OPENSSL_SRC%\..\build\Win32\include
 set LIB=%LIB%;%OPENSSL_SRC%\..\build\Win32\lib
@@ -26,8 +26,23 @@ nmake -f ms\ntdll.mak install
 rmdir /q /s %OPENSSL_DEST%
 mkdir %OPENSSL_DEST%
 move bin %OPENSSL_DEST%
+copy out32dll\libeay32.pdb %OPENSSL_DEST%\bin
+copy out32dll\openssl.pdb %OPENSSL_DEST%\bin
+copy out32dll\ssleay32.pdb %OPENSSL_DEST%\bin
 move include %OPENSSL_DEST%
 move lib %OPENSSL_DEST%
+copy out32dll\4758cca.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\aep.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\atalla.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\capi.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\chil.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\cswift.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\gmp.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\gost.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\nuron.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\padlock.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\sureware.pdb %OPENSSL_DEST%\lib\engines
+copy out32dll\ubsec.pdb %OPENSSL_DEST%\lib\engines
 mkdir %OPENSSL_DEST%\share
 mkdir %OPENSSL_DEST%\share\doc
 mkdir %OPENSSL_DEST%\share\doc\openssl
