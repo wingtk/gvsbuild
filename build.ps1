66523d4c2c3dff030c016b9d6324020bed1f832c
tomek <eustachy.kapusta@gmail.com>
2014-08-07 22:35:09 +0200

    openssl: Updated to version 1.0.1i

--- build.ps1
+++ build.ps1
@@ -140,7 +140,7 @@ $items = @{
 	'libffi'           = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/libffi-3.0.13.7z';        'Dependencies' = @()                                    };
 	'libpng'           = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/libpng-1.6.12.7z';        'Dependencies' = @('zlib')                              };
 	'libxml2'          = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/libxml2-2.9.1.7z';        'Dependencies' = @('win-iconv')                         };
-	'openssl'          = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/openssl-1.0.1h.7z';       'Dependencies' = @('zlib')                              };
+	'openssl'          = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/openssl-1.0.1i.7z';       'Dependencies' = @('zlib')                              };
 	'pango'            = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/pango-1.36.5.7z';         'Dependencies' = @('cairo', 'harfbuzz')                 };
 	'pixman'           = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/pixman-0.32.6.7z';        'Dependencies' = @('libpng')                            };
 	'win-iconv'        = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/win-iconv-0.0.6.7z';      'Dependencies' = @()                                    };
--- openssl/mod.md
+++ openssl/mod.md
@@ -1,4 +1,4 @@
- * Download [OpenSSL 1.0.1h](http://www.openssl.org/source/openssl-1.0.1h.tar.gz)
+ * Download [OpenSSL 1.0.1h](http://www.openssl.org/source/openssl-1.0.1i.tar.gz)
  * Download the latest [NSS](http://hg.mozilla.org/mozilla-central/) certificate list (`security/nss/lib/ckfw/builtins/certdata.txt`)
  * Extract to `C:\mozilla-build\hexchat`
  * Open VS x86 command prompt
