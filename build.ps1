f6a3fe872e6586b3fd0b1d499a8b01f74f4b0bf8
Arnavion <arnavion@gmail.com>
2015-03-15 22:14:37 -0700

    Rearrange source URLs to not require reformatting all the rows.

--- build.ps1
+++ build.ps1
@@ -135,24 +135,95 @@ param (
 #========================================================================================================================================================
 
 $items = @{
-	'atk'              = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/atk-2.14.0.tar.xz';        'Dependencies' = @('glib')                              };
-	'cairo'            = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/cairo-1.14.0.tar.xz';      'Dependencies' = @('fontconfig', 'glib', 'pixman')      };
-	'enchant'          = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/enchant-1.6.0.tar.gz';     'Dependencies' = @('glib')                              };
-	'fontconfig'       = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/fontconfig-2.8.0.tar.gz';  'Dependencies' = @('freetype', 'libxml2')               };
-	'freetype'         = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/freetype-2.5.5.tar.bz2';   'Dependencies' = @()                                    };
-	'gdk-pixbuf'       = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/gdk-pixbuf-2.30.8.tar.xz'; 'Dependencies' = @('glib', 'libpng')                    };
-	'gettext-runtime'  = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/gettext-runtime-0.18.7z';  'Dependencies' = @('win-iconv')                         };
-	'glib'             = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/glib-2.42.2.tar.xz';       'Dependencies' = @('gettext-runtime', 'libffi', 'zlib') };
-	'gtk'              = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/gtk+-2.24.27.tar.xz';      'Dependencies' = @('atk', 'gdk-pixbuf', 'pango')        };
-	'harfbuzz'         = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/harfbuzz-0.9.39.tar.bz2';  'Dependencies' = @('freetype', 'glib')                  };
-	'libffi'           = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/libffi-3.0.13.tar.gz';     'Dependencies' = @()                                    };
-	'libpng'           = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/libpng-1.6.16.tar.xz';     'Dependencies' = @('zlib')                              };
-	'libxml2'          = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/libxml2-2.9.1.tar.gz';     'Dependencies' = @('win-iconv')                         };
-	'openssl'          = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/openssl-1.0.1l.tar.gz';    'Dependencies' = @()                                    };
-	'pango'            = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/pango-1.36.8.tar.xz';      'Dependencies' = @('cairo', 'harfbuzz')                 };
-	'pixman'           = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/pixman-0.32.6.tar.gz';     'Dependencies' = @('libpng')                            };
-	'win-iconv'        = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/win-iconv-0.0.6.tar.bz2';  'Dependencies' = @()                                    };
-	'zlib'             = @{ 'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/zlib-1.2.8.tar.xz';        'Dependencies' = @()                                    };
+	'atk' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/atk-2.14.0.tar.xz'
+		'Dependencies' = @('glib')
+	};
+
+	'cairo' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/cairo-1.14.0.tar.xz'
+		'Dependencies' = @('fontconfig', 'glib', 'pixman')
+	};
+
+	'enchant' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/enchant-1.6.0.tar.gz'
+		'Dependencies' = @('glib')
+	};
+
+	'fontconfig' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/fontconfig-2.8.0.tar.gz'
+		'Dependencies' = @('freetype', 'libxml2')
+	};
+
+	'freetype' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/freetype-2.5.5.tar.bz2'
+		'Dependencies' = @()
+	};
+
+	'gdk-pixbuf' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/gdk-pixbuf-2.30.8.tar.xz'
+		'Dependencies' = @('glib', 'libpng')
+	};
+
+	'gettext-runtime' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/gettext-runtime-0.18.7z'
+		'Dependencies' = @('win-iconv')
+	};
+
+	'glib' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/glib-2.42.2.tar.xz'
+		'Dependencies' = @('gettext-runtime', 'libffi', 'zlib')
+	};
+
+	'gtk' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/gtk+-2.24.27.tar.xz'
+		'Dependencies' = @('atk', 'gdk-pixbuf', 'pango')
+	};
+
+	'harfbuzz' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/harfbuzz-0.9.39.tar.bz2'
+		'Dependencies' = @('freetype', 'glib')
+	};
+
+	'libffi' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/libffi-3.0.13.tar.gz'
+		'Dependencies' = @()
+	};
+
+	'libpng' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/libpng-1.6.16.tar.xz'
+		'Dependencies' = @('zlib')
+	};
+
+	'libxml2' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/libxml2-2.9.1.tar.gz'
+		'Dependencies' = @('win-iconv')
+	};
+
+	'openssl' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/openssl-1.0.1l.tar.gz'
+		'Dependencies' = @()
+	};
+
+	'pango' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/pango-1.36.8.tar.xz'
+		'Dependencies' = @('cairo', 'harfbuzz')
+	};
+
+	'pixman' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/pixman-0.32.6.tar.gz'
+		'Dependencies' = @('libpng')
+	};
+
+	'win-iconv' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/win-iconv-0.0.6.tar.bz2'
+		'Dependencies' = @()
+	};
+
+	'zlib' = @{
+		'ArchiveUrl' = 'http://dl.hexchat.net/gtk-win32/src/zlib-1.2.8.tar.xz'
+		'Dependencies' = @()
+	};
 }
 
 #========================================================================================================================================================
