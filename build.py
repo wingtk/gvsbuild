1da8a5120f966745c5ecbe0327d1681108e45c82
Ignacio Casal Quinteiro <ignacio.casal@nice-software.com>
2016-03-30 14:41:48 +0200

    openssl: update to version 1.0.2g

--- build.py
+++ build.py
@@ -638,7 +638,7 @@ class Project_openssl(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
             'openssl',
-            archive_url = 'ftp://ftp.openssl.org/source/openssl-1.0.2f.tar.gz',
+            archive_url = 'ftp://ftp.openssl.org/source/openssl-1.0.2g.tar.gz',
             )
 
     def build(self):
