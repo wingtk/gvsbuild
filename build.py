0dc602621fa113ab55e1e402738022535139cb14
Ignacio Casal Quinteiro <ignacio.casal@nice-software.com>
2016-03-31 11:32:21 +0200

    glib-networking: update to version 2.48.0

--- build.py
+++ build.py
@@ -402,7 +402,7 @@ class Project_glib_networking(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
             'glib-networking',
-            archive_url = 'https://github.com/wingtk/glib-networking/releases/download/2.46.3-openssl/glib-networking-2.46.3.tar.xz',
+            archive_url = 'https://github.com/wingtk/glib-networking/releases/download/2.48.0-openssl/glib-networking-2.48.0.tar.xz',
             dependencies = ['gsettings-desktop-schemas', 'openssl'],
             )
 
