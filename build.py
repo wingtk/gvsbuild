e081fd8e840e261d25bd1cd86e86db851188b0bc
Ignacio Casal Quinteiro <ignacio.casal@nice-software.com>
2016-05-10 03:53:22 -0700

    gtk3: update to version 3.20.4

--- build.py
+++ build.py
@@ -466,7 +466,7 @@ class Project_gtk3(Project_gtk_base):
     def __init__(self):
         Project_gtk_base.__init__(self,
             'gtk3',
-            archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/gtk+/3.20/gtk+-3.20.3.tar.xz',
+            archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/gtk+/3.20/gtk+-3.20.4.tar.xz',
             dependencies = ['atk', 'gdk-pixbuf', 'pango', 'libepoxy'],
             )
 
