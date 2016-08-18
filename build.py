d1fc17bdd626af94009e94b031cf5d3a18bf1b12
Ignacio Casal Quinteiro <ignacio.casal@nice-software.com>
2016-08-18 08:58:28 +0200

    gtk3: update to version 3.20.9

--- build.py
+++ build.py
@@ -582,9 +582,8 @@ class Project_gtk3(Project_gtk_base):
     def __init__(self):
         Project_gtk_base.__init__(self,
             'gtk3',
-            archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/gtk+/3.20/gtk+-3.20.8.tar.xz',
+            archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/gtk+/3.20/gtk+-3.20.9.tar.xz',
             dependencies = ['atk', 'gdk-pixbuf', 'pango', 'libepoxy'],
-            patches = ['0001-Use-g_snprintf-instead-of-snprintf.patch'],
             )
 
     def build(self):
