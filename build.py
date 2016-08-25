4dd44e922cc456fcad18994ee1b3b7875f2e372d
Chungrae Cho <hsccr@amk.ne.kr>
2016-08-25 16:57:20 +0900

    Add libssh2 project

--- build.py
+++ build.py
@@ -893,6 +893,23 @@ class Project_libssh(Tarball, Project):
 
 Project.add(Project_libssh())
 
+class Project_libssh2(Tarball, Project):
+    def __init__(self):
+        Project.__init__(self,
+            'libssh2',
+            archive_url = 'https://www.libssh2.org/download/libssh2-1.7.0.tar.gz',
+            )
+
+    def build(self):
+        cmake_config = 'Debug' if self.builder.opts.configuration == 'debug' else 'RelWithDebInfo'
+        self.exec_vs(r'cmake -G"NMake Makefiles" -DCMAKE_INSTALL_PREFIX="%(gtk_dir)s" -DGTK_DIR="%(pkg_dir)s" -DWITH_ZLIB=ON -DCMAKE_BUILD_TYPE=' + cmake_config, add_path=self.builder.opts.cmake_path)
+        self.exec_vs(r'nmake /nologo', add_path=self.builder.opts.cmake_path)
+        self.exec_vs(r'nmake /nologo install', add_path=self.builder.opts.cmake_path)
+
+        self.install(r'.\COPYING share\doc\libssh2')
+
+Project.add(Project_libssh2())
+
 class Project_libuv(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
