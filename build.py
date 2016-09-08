c2184694fe19c51bee28281ba133dfbc7846952c
Ignacio Casal Quinteiro <ignacio.casal@nice-software.com>
2016-09-08 16:30:28 +0200

    pango: update to version 1.40.2

--- build.py
+++ build.py
@@ -1143,7 +1143,7 @@ class Project_pango(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
             'pango',
-            archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/pango/1.40/pango-1.40.1.tar.xz',
+            archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/pango/1.40/pango-1.40.2.tar.xz',
             dependencies = ['cairo', 'harfbuzz'],
             )
 
