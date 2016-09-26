6c4bab8d111197925d648f22aa65b9c2aba7a25f
Ignacio Casal Quinteiro <ignacio.casal@nice-software.com>
2016-09-26 15:41:56 +0200

    freetype: update to version 2.7

--- build.py
+++ build.py
@@ -439,7 +439,7 @@ class Project_freetype(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
             'freetype',
-            archive_url = 'http://download.savannah.gnu.org/releases/freetype/freetype-2.6.3.tar.bz2',
+            archive_url = 'http://download.savannah.gnu.org/releases/freetype/freetype-2.7.tar.gz',
             )
 
     def build(self):
