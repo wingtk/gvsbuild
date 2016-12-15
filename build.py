cfe5c3a00854f135c7f54aa81203b4e11928fda8
Ignacio Casal Quinteiro <ignacio.casal@nice-software.com>
2016-12-15 22:04:39 +0100

    gtk3: update to version 3.22.5

--- build.py
+++ build.py
@@ -647,7 +647,7 @@ class Project_gtk3(Project_gtk_base):
     def __init__(self):
         Project_gtk_base.__init__(self,
             'gtk3',
-            archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/gtk+/3.22/gtk+-3.22.3.tar.xz',
+            archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/gtk+/3.22/gtk+-3.22.5.tar.xz',
             dependencies = ['atk', 'gdk-pixbuf', 'pango', 'libepoxy'],
             patches = ['gtk3-clip-retry-if-opened-by-others.patch'],
             )
