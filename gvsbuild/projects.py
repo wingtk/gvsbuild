d877b75b4586f2537b363fe7bd7664648a27edef
Ignacio Casal Quinteiro <qignacio@amazon.com>
2017-05-16 09:21:12 +0200

    gsettings-desktop-schemas: add patch to fix the build with py3

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -487,6 +487,7 @@ class Project_gsettings_desktop_schemas(Tarball, Project):
             archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/gsettings-desktop-schemas/3.24/gsettings-desktop-schemas-3.24.0.tar.xz',
             hash = 'f6573a3f661d22ff8a001cc2421d8647717f1c0e697e342d03c6102f29bbbb90',
             dependencies = ['perl', 'glib'],
+            patches = ['0001-build-win32-replace.py-Fix-replacing-items-in-files-.patch'],
             )
 
     def build(self):
