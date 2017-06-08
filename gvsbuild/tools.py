45a7b66e8c39fc3b81ec1589717dd9df39984de0
Danny 'Guru' Forghieri <daniele.forghieri@gmail.com>
2017-06-08 08:48:19 +0200

    Download the exact version of nuget, to avoid hash mismatch when they upgraded it

--- gvsbuild/tools.py
+++ gvsbuild/tools.py
@@ -115,7 +115,7 @@ class Tool_nuget(Tool):
     def __init__(self):
         Tool.__init__(self,
             'nuget',
-            archive_url = 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe',
+            archive_url = 'https://dist.nuget.org/win-x86-commandline/v3.5.0/nuget.exe',
             hash = '399ec24c26ed54d6887cde61994bb3d1cada7956c1b19ff880f06f060c039918')
 
     def load_defaults(self, builder):
