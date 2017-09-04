dee5c87cc4a2863eed2bcae402af4b9a01d01eb0
Danny 'Guru' Forghieri <daniele.forghieri@gmail.com>
2017-09-04 23:08:31 +0200

    Project to verify the correct use of the pkgconfig .pc files.
    It simply, using meson & pkg-config, compile an executable that use
    and print the address of one single symbol in the library.

---
+++ check-libs/test_libjpeg.c
@@ -0,0 +1,10 @@
+// test for libjpeg
+
+// include
+    // standard
+    #include "check_utils.h"
+    // library
+    #include <jpeglib.h>
+
+// check one function from the dll
+CHECK_ONE(jpeg_CreateCompress)
---
+++ check-libs/test_libtiff-4.c
@@ -0,0 +1,10 @@
+// test for libtiff-4
+
+// include
+    // standard
+    #include "check_utils.h"
+    // library
+    #include <tiffio.h>
+
+// check one function from the dll
+CHECK_ONE(TIFFCleanup)
