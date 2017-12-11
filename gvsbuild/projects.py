f6ce5f35f1b889391a8d18bf8fcee9d9aa855600
Ignacio Casal Quinteiro <qignacio@amazon.com>
2017-12-11 11:58:48 +0100

    pango: add patch from upstream
    This is required because of the new freetype release

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -1154,6 +1154,7 @@ class Project_pango(Tarball, Project):
             archive_url = 'http://ftp.acc.umu.se/pub/GNOME/sources/pango/1.40/pango-1.40.14.tar.xz',
             hash = '90af1beaa7bf9e4c52db29ec251ec4fd0a8f2cc185d521ad1f88d01b3a6a17e3',
             dependencies = ['cairo', 'harfbuzz'],
+            patches = ['0001-Drop-unused-_pango_ft2_ft_strerror.patch'],
             )
 
     def build(self):
