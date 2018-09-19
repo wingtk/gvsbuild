a49e7456d7de6424caca99ac97c98bdbdbcbbefd
Ignacio Casal Quinteiro <qignacio@amazon.com>
2018-09-19 16:17:47 +0200

    lz4: update to version 1.8.3

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -1274,9 +1274,9 @@ class Project_lz4(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
             'lz4',
-            archive_url = 'https://github.com/lz4/lz4/archive/v1.8.2.tar.gz',
-            archive_file_name = 'lz4-1.8.2.tar.gz',
-            hash = '0963fbe9ee90acd1d15e9f09e826eaaf8ea0312e854803caf2db0a6dd40f4464',
+            archive_url = 'https://github.com/lz4/lz4/archive/v1.8.3.tar.gz',
+            archive_file_name = 'lz4-1.8.3.tar.gz',
+            hash = '33af5936ac06536805f9745e0b6d61da606a1f8b4cc5c04dd3cbaca3b9b4fc43',
             )
 
     def build(self):
