5947dd0f0d5afa8a6c65dee3234c001b777966d5
Marco A <alesiani@amazon.com>
2018-09-20 10:59:07 +0200

    Add MIT Kerberos support as Cyrus SASL provider

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -160,15 +160,17 @@ class Project_cyrus_sasl(Tarball, Project):
             'cyrus-sasl',
             hash = '9e8035c12d419209ea60584d5efa51d042c3ed44b450b9d173d5504b222df9f1',
             archive_url = 'https://github.com/wingtk/cyrus-sasl/releases/download/cyrus-sasl-lmdb-2.1.28/cyrus-sasl-2.1.28.tar.xz',
-            dependencies = ['lmdb', 'openssl'],
+            dependencies = ['lmdb', 'openssl', 'mit-kerberos'],
             patches = ['0001-fix-snprintf-macro.patch'],
             )
 
     def build(self):
         configuration = 'Debug' if self.builder.opts.configuration == 'debug' else 'Release'
         self.exec_vs(r'nmake /nologo /f NTMakefile SASLDB="LMDB" LMDB_INCLUDE="%(gtk_dir)s\include" LMDB_LIBPATH="%(gtk_dir)s\lib" ' +
+                     r'GSSAPI="MITKerberos" GSSAPI_INCLUDE="%(gtk_dir)s\include" GSSAPI_LIBPATH="%(gtk_dir)s\lib" ' +
                      r'OPENSSL_INCLUDE="%(gtk_dir)s\include" OPENSSL_LIBPATH="%(gtk_dir)s\lib" prefix="%(pkg_dir)s" CFG=' + configuration)
         self.exec_vs(r'nmake /nologo /f NTMakefile install SASLDB="LMDB" LMDB_INCLUDE="%(gtk_dir)s\include" ' +
+                     r'GSSAPI="MITKerberos" GSSAPI_INCLUDE="%(gtk_dir)s\include" GSSAPI_LIBPATH="%(gtk_dir)s\lib" ' +
                      r'LMDB_LIBPATH="%(gtk_dir)s\lib" OPENSSL_INCLUDE="%(gtk_dir)s\include" OPENSSL_LIBPATH="%(gtk_dir)s\lib" prefix="%(pkg_dir)s" CFG=' + configuration)
 
         self.install(r'.\COPYING share\doc\cyrus-sasl')
