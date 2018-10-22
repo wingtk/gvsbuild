9d11ad6a008e6939dd1f169427f0636904e5fc07
Ignacio Casal Quinteiro <qignacio@amazon.com>
2018-10-22 17:01:28 +0200

    libepoxy: fix download url

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -939,7 +939,7 @@ class Project_libepoxy(Tarball, Meson):
     def __init__(self):
         Project.__init__(self,
             'libepoxy',
-            archive_url = 'https://github.com/anholt/libepoxy/releases/download/1.5.2/libepoxy-1.5.3.tar.xz',
+            archive_url = 'https://github.com/anholt/libepoxy/releases/download/1.5.3/libepoxy-1.5.3.tar.xz',
             hash = '002958c5528321edd53440235d3c44e71b5b1e09b9177e8daf677450b6c4433d',
             dependencies = ['python', 'ninja', 'meson'],
             )
