1882f37f012cf3901eaeb9ccb8ae390652461f6a
Ignacio Casal Quinteiro <qignacio@amazon.com>
2018-10-22 09:42:08 +0200

    opus: update to version 1.3

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -1382,8 +1382,8 @@ class Project_opus(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
             'opus',
-            archive_url = 'https://archive.mozilla.org/pub/opus/opus-1.2.1.tar.gz',
-            hash = 'cfafd339ccd9c5ef8d6ab15d7e1a412c054bf4cb4ecbbbcc78c12ef2def70732',
+            archive_url = 'https://archive.mozilla.org/pub/opus/opus-1.3.tar.gz',
+            hash = '4f3d69aefdf2dbaf9825408e452a8a414ffc60494c70633560700398820dc550',
             )
 
     def build(self):
