5e40fee4c0d742627bffdd0bd7403dfbec711f9f
Ignacio Casal Quinteiro <qignacio@amazon.com>
2018-10-23 11:12:40 +0200

    cairo: update to version 1.16.0

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -81,10 +81,9 @@ class Project_cairo(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
             'cairo',
-            archive_url = 'http://cairographics.org/snapshots/cairo-1.15.2.tar.xz',
-            hash = '268cc265a7f807403582f440643064bf52896556766890c8df7bad02d230f6c9',
+            archive_url = 'https://cairographics.org/releases/cairo-1.16.0.tar.xz',
+            hash = '5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331',
             dependencies = ['fontconfig', 'glib', 'pixman', 'libpng'],
-            patches = ['cairo.patch']
             )
 
     def build(self):
