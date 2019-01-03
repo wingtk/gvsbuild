6fbb1140b94a3b78f03619979a1d461cb88f060f
guruDanny67 <daniele.forghieri@gmail.com>
2019-01-03 11:08:51 +0100

    We can pass global options to the ninja build

--- gvsbuild/utils/parser.py
+++ gvsbuild/utils/parser.py
@@ -65,6 +65,7 @@ def get_options(args):
     opts.ffmpeg_enable_gpl = args.ffmpeg_enable_gpl
     opts.log_size = args.log_size
     opts.log_single = args.log_single
+    opts.ninja_opts = args.ninja_opts
 
     # active the log
     log.configure(os.path.join(opts.build_dir, 'logs'), opts)
@@ -282,6 +283,8 @@ Examples:
                          help="Maximum log size (in kilobytes) before restarting with a new file")
     p_build.add_argument('--log-single', default=False, action='store_true',
                          help="Always start a new log file, with date & time")
+    p_build.add_argument('--ninja-opts', default='',
+                         help='Command line options to pass to ninja, e.g. to limit the use (-j 2) or for debug purpouse.')
 
     p_build.add_argument('project', nargs='+',
                          help='Project(s) to build.')
