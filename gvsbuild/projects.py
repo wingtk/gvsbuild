5388581d14befe0de278df42941caaa90a1eaedd
Ignacio Casal Quinteiro <qignacio@amazon.com>
2020-01-29 15:40:13 +0100

    libjpeg-turbo: update to version 2.0.4

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -1108,8 +1108,8 @@ class Project_libjpeg_turbo(Tarball, CmakeProject):
     def __init__(self):
         Project.__init__(self,
             'libjpeg-turbo',
-            archive_url = 'https://sourceforge.net/projects/libjpeg-turbo/files/2.0.3/libjpeg-turbo-2.0.3.tar.gz',
-            hash = '4246de500544d4ee408ee57048aa4aadc6f165fc17f141da87669f20ed3241b7',
+            archive_url = 'https://sourceforge.net/projects/libjpeg-turbo/files/2.0.4/libjpeg-turbo-2.0.4.tar.gz',
+            hash = '33dd8547efd5543639e890efbf2ef52d5a21df81faf41bb940657af916a23406',
             dependencies = ['cmake', 'ninja', 'nasm', ],
             )
 
