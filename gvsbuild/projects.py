08366a62a6c58c0d853891849e009cf08a43a60f
Ignacio Casal Quinteiro <qignacio@amazon.com>
2020-11-24 13:00:45 +0100

    pixman: update to version 0.40.0

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -1738,8 +1738,8 @@ class Project_pixman(Tarball, Project):
     def __init__(self):
         Project.__init__(self,
             'pixman',
-            archive_url = 'http://cairographics.org/releases/pixman-0.38.4.tar.gz',
-            hash = 'da66d6fd6e40aee70f7bd02e4f8f76fc3f006ec879d346bae6a723025cfbdde7',
+            archive_url = 'http://cairographics.org/releases/pixman-0.40.0.tar.gz',
+            hash = '6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc',
             )
 
     def build(self):
