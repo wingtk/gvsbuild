6c1689fe9f53e07d5762567ec74932859370c872
Ignacio Casal Quinteiro <qignacio@amazon.com>
2021-01-26 10:50:35 +0100

    cairo: add patch to fix security issue

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -87,6 +87,7 @@ class Project_cairo(Tarball, Project):
             archive_url = 'https://cairographics.org/releases/cairo-1.16.0.tar.xz',
             hash = '5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331',
             dependencies = ['fontconfig', 'glib', 'pixman', 'libpng'],
+            patches = ['0001-Fix-mask-usage-in-image-compositor.patch'],
             )
 
     def build(self):
