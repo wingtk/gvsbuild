d36ba28a6658ab932819bacabe9f931830ec327a
Dan Yeaw <dan@yeaw.me>
2021-12-30 21:14:23 -0500

    Fix errors and enable shellcheck pre-commit

--- patches/ffmpeg/build/build.sh
+++ patches/ffmpeg/build/build.sh
@@ -1,3 +1,5 @@
+#!/usr/bash
+
 prefix="$1"
 gtk_dir="$2"
 build_type="$3"
@@ -19,34 +21,34 @@ fi
 export PKG_CONFIG_PATH=$gtk_dir/lib/pkgconfig:$PKG_CONFIG_PATH
 
 ./configure \
-    --toolchain=msvc \
-    --prefix="$prefix" \
-    --enable-shared \
-    --disable-everything \
-    --enable-swscale \
-    --enable-avcodec \
-    --enable-hwaccel=h264_dxva2 \
-    --enable-hwaccel=hevc_dxva2 \
-    --enable-dxva2 \
-    --enable-decoder=h264 \
-    --enable-decoder=hevc \
-    --enable-decoder=mpeg1video \
-    --enable-encoder=mpeg1video \
-    --enable-hwaccel=h264_d3d11va \
-    --enable-hwaccel=h264_d3d11va2 \
-    --enable-hwaccel=hevc_d3d11va \
-    --enable-hwaccel=hevc_d3d11va2 \
-    --enable-d3d11va \
-    --enable-nvdec \
-    --enable-hwaccel=h264_nvdec \
-    --enable-hwaccel=hevc_nvdec \
-    --disable-programs \
-    --disable-avformat \
-    --disable-avfilter \
-    --disable-avdevice \
-    --disable-swresample \
-    --extra-cflags="$extra_cflags" \
-    $extra_flags
+ --toolchain=msvc \
+ --prefix="$prefix" \
+ --enable-shared \
+ --disable-everything \
+ --enable-swscale \
+ --enable-avcodec \
+ --enable-hwaccel=h264_dxva2 \
+ --enable-hwaccel=hevc_dxva2 \
+ --enable-dxva2 \
+ --enable-decoder=h264 \
+ --enable-decoder=hevc \
+ --enable-decoder=mpeg1video \
+ --enable-encoder=mpeg1video \
+ --enable-hwaccel=h264_d3d11va \
+ --enable-hwaccel=h264_d3d11va2 \
+ --enable-hwaccel=hevc_d3d11va \
+ --enable-hwaccel=hevc_d3d11va2 \
+ --enable-d3d11va \
+ --enable-nvdec \
+ --enable-hwaccel=h264_nvdec \
+ --enable-hwaccel=hevc_nvdec \
+ --disable-programs \
+ --disable-avformat \
+ --disable-avfilter \
+ --disable-avdevice \
+ --disable-swresample \
+ --extra-cflags="$extra_cflags" \
+ "$extra_flags"
 
 make
 make install
