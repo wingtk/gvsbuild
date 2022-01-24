088b9a979e217a51e5060bb43e6a69ce947150ad
Ignacio Casal Quinteiro <qignacio@amazon.com>
2022-01-24 11:57:00 +0100

    ffmpeg: try to fix shell check

--- patches/ffmpeg/build/build.sh
+++ patches/ffmpeg/build/build.sh
@@ -5,50 +5,52 @@ gtk_dir="$2"
 build_type="$3"
 enable_gpl="$4"
 
-extra_cflags=""
-extra_flags=""
+declare -a configure_cmd
+declare -i idx=0
+
+configure_cmd[idx++]="./configure"
+configure_cmd[idx++]="--toolchain=msvc"
+configure_cmd[idx++]="--prefix=\"$prefix\""
+configure_cmd[idx++]="--enable-shared"
+configure_cmd[idx++]="--disable-everything"
+configure_cmd[idx++]="--enable-swscale"
+configure_cmd[idx++]="--enable-avcodec"
+configure_cmd[idx++]="--enable-hwaccel=h264_dxva2"
+configure_cmd[idx++]="--enable-hwaccel=hevc_dxva2"
+configure_cmd[idx++]="--enable-dxva2"
+configure_cmd[idx++]="--enable-decoder=h264"
+configure_cmd[idx++]="--enable-decoder=hevc"
+configure_cmd[idx++]="--enable-decoder=mpeg1video"
+configure_cmd[idx++]="--enable-encoder=mpeg1video"
+configure_cmd[idx++]="--enable-hwaccel=h264_d3d11va"
+configure_cmd[idx++]="--enable-hwaccel=h264_d3d11va2"
+configure_cmd[idx++]="--enable-hwaccel=hevc_d3d11va"
+configure_cmd[idx++]="--enable-hwaccel=hevc_d3d11va2"
+configure_cmd[idx++]="--enable-d3d11va"
+configure_cmd[idx++]="--enable-nvdec"
+configure_cmd[idx++]="--enable-hwaccel=h264_nvdec"
+configure_cmd[idx++]="--enable-hwaccel=hevc_nvdec"
+configure_cmd[idx++]="--disable-programs"
+configure_cmd[idx++]="--disable-avformat"
+configure_cmd[idx++]="--disable-avfilter"
+configure_cmd[idx++]="--disable-avdevice"
+configure_cmd[idx++]="--disable-swresample"
 
 if [ "$build_type" = "debug" ]; then
-    extra_flags="--enable-debug $extra_flags"
+    configure_cmd[idx++]="--enable-debug"
     # FIXME: the -Od and -Zi instructions are overriden in the compilation command
-    extra_cflags="-Od -Zi -MDd $extra_cflags"
+    configure_cmd[idx++]="--extra-cflags=\"-Od -Zi -MDd\""
 fi
 
 if [ "$enable_gpl" = "enable_gpl" ]; then
-    extra_flags="--enable-libx264 --enable-gpl --enable-encoder=\"libx264\" $extra_flags"
+    configure_cmd[idx++]="--enable-libx264"
+    configure_cmd[idx++]="--enable-gpl"
+    configure_cmd[idx++]="--enable-encoder=\"libx264\""
 fi
 
 export PKG_CONFIG_PATH=$gtk_dir/lib/pkgconfig:$PKG_CONFIG_PATH
 
-./configure \
- --toolchain=msvc \
- --prefix="$prefix" \
- --enable-shared \
- --disable-everything \
- --enable-swscale \
- --enable-avcodec \
- --enable-hwaccel=h264_dxva2 \
- --enable-hwaccel=hevc_dxva2 \
- --enable-dxva2 \
- --enable-decoder=h264 \
- --enable-decoder=hevc \
- --enable-decoder=mpeg1video \
- --enable-encoder=mpeg1video \
- --enable-hwaccel=h264_d3d11va \
- --enable-hwaccel=h264_d3d11va2 \
- --enable-hwaccel=hevc_d3d11va \
- --enable-hwaccel=hevc_d3d11va2 \
- --enable-d3d11va \
- --enable-nvdec \
- --enable-hwaccel=h264_nvdec \
- --enable-hwaccel=hevc_nvdec \
- --disable-programs \
- --disable-avformat \
- --disable-avfilter \
- --disable-avdevice \
- --disable-swresample \
- --extra-cflags="$extra_cflags" \
- $extra_flags
+"${configure_cmd[@]}"
 
 make
 make install
