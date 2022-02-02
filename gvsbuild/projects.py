a9941024f19f046cb5e4e27d60dc51928537826d
Ignacio Casal Quinteiro <qignacio@amazon.com>
2022-02-02 09:33:07 +0100

    Revert "ffmpeg: update to version 4.4.1"
    This reverts commit 260b698e3dba68414dd102935a5b7afa70db0d88.
    
    For some reason avcodec.lib is not there anymore with
    this update.

--- gvsbuild/projects.py
+++ gvsbuild/projects.py
@@ -360,8 +360,8 @@ class Project_ffmpeg(Tarball, Project):
         Project.__init__(
             self,
             "ffmpeg",
-            archive_url="https://www.ffmpeg.org/releases/ffmpeg-4.4.1.tar.xz",
-            hash="eadbad9e9ab30b25f5520fbfde99fae4a92a1ae3c0257a8d68569a4651e30e02",
+            archive_url="https://www.ffmpeg.org/releases/ffmpeg-4.4.tar.xz",
+            hash="06b10a183ce5371f915c6bb15b7b1fffbe046e8275099c96affc29e17645d909",
             dependencies=["nasm", "msys2", "pkg-config", "nv-codec-headers"],
         )
         if self.opts.ffmpeg_enable_gpl:
