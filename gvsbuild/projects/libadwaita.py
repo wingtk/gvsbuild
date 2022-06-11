e7d0f096c83417890f215e4105a5aebee44658e8
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-11 19:43:20 +0700

    libadwaita: add Nefo Fortressia as an author

--- gvsbuild/projects/libadwaita.py
+++ gvsbuild/projects/libadwaita.py
@@ -1,6 +1,7 @@
 #  Copyright (C) 2016 - Yevgen Muntyan
 #  Copyright (C) 2016 - Ignacio Casal Quinteiro
 #  Copyright (C) 2016 - Arnavion
+#  Copyright (C) 2022 - Nefo Fortressia
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
