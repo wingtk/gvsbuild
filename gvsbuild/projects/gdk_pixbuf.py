99ef9dedf8467a040d51a538a31a44cf5ca94ff4
Martin Hertz <mvhertz@gmail.com>
2022-06-14 20:25:35 +0200

    gdk-pixbuf: Use 'builtin_loaders=all'
    Replace 'native_windows_loaders=true' with 'builtin_loaders=all' to fix issue of Pixbuf.new_from_file crashes loading gif, ico or bmp images

--- gvsbuild/projects/gdk_pixbuf.py
+++ gvsbuild/projects/gdk_pixbuf.py
@@ -34,6 +34,7 @@ class GdkPixbuf(Tarball, Meson):
                 "meson",
                 "python",
                 "libtiff-4",
+                "libjpeg-turbo",
                 "glib",
                 "libpng",
             ],
@@ -44,7 +45,7 @@ class GdkPixbuf(Tarball, Meson):
         else:
             enable_gi = "disabled"
 
-        self.add_param("-Dnative_windows_loaders=true")
+        self.add_param("-Dbuiltin_loaders=all")
         self.add_param(f"-Dintrospection={enable_gi}")
         self.add_param("-Dman=false")
 
