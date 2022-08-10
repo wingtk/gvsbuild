0f669ffdf5ceb1ceebe4bb2e1d23537bcc57d935
Ignacio Casal Quinteiro <qignacio@amazon.com>
2022-08-10 17:03:17 +0200

    wing: update to version 0.3.18

--- gvsbuild/projects/wing.py
+++ gvsbuild/projects/wing.py
@@ -26,8 +26,8 @@ class Wing(Tarball, Meson):
         Project.__init__(
             self,
             "wing",
-            archive_url="https://gitlab.gnome.org/GNOME/wing/-/archive/v0.3.16/wing-v0.3.16.tar.gz",
-            hash="adc881345acc13a4db8dbee28b178989175c3f3d5174e423b021a5a29c61225c",
+            archive_url="https://gitlab.gnome.org/GNOME/wing/-/archive/v0.3.18/wing-v0.3.18.tar.gz",
+            hash="6d15984c917d9bdf2c88a06072991daf39a226d2024ec5b196a1c9ed8e81e962",
             dependencies=["ninja", "meson", "pkg-config", "glib"],
         )
 
