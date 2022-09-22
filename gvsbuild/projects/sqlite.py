d523ace0555b122462b3d24b721020d28c75f166
Paolo Borelli <pborelli@gnome.org>
2022-09-22 10:36:33 +0200

    Update sqlite to 3.39.3

--- gvsbuild/projects/sqlite.py
+++ gvsbuild/projects/sqlite.py
@@ -25,8 +25,8 @@ class SQLite(Tarball, Project):
         Project.__init__(
             self,
             "sqlite",
-            archive_url="https://www.sqlite.org/2022/sqlite-autoconf-3390200.tar.gz",
-            hash="852be8a6183a17ba47cee0bbff7400b7aa5affd283bf3beefc34fcd088a239de",
+            archive_url="https://www.sqlite.org/2022/sqlite-autoconf-3390300.tar.gz",
+            hash="7868fb3082be3f2cf4491c6fba6de2bddcbc293a35fefb0624ee3c13f01422b9",
         )
 
     def build(self):
