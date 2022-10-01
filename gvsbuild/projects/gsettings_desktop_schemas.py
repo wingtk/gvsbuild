d6ff7aa7e96e7c79c8047d886b03fa50ae9199de
Dan Yeaw <dan@yeaw.me>
2022-09-30 20:51:58 -0400

    Update gsettings-desktop-schemas to version 43.0

--- gvsbuild/projects/gsettings_desktop_schemas.py
+++ gvsbuild/projects/gsettings_desktop_schemas.py
@@ -26,8 +26,8 @@ class GSettingsDesktopSchemas(Tarball, Meson):
         Project.__init__(
             self,
             "gsettings-desktop-schemas",
-            archive_url="https://download.gnome.org/sources/gsettings-desktop-schemas/42/gsettings-desktop-schemas-42.0.tar.xz",
-            hash="6686335a9ed623f7ae2276fefa50a410d4e71d4231880824714070cb317323d2",
+            archive_url="https://download.gnome.org/sources/gsettings-desktop-schemas/43/gsettings-desktop-schemas-43.0.tar.xz",
+            hash="5d5568282ab38b95759d425401f7476e56f8cbf2629885587439f43bd0b84bbe",
             dependencies=["meson", "ninja", "pkg-config", "python", "glib"],
         )
         if self.opts.enable_gi:
