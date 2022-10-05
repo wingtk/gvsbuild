d6fcfd67e29018245ce9322954d3c097e41bf546
Alexandros Theodotou <alex@zrythm.org>
2022-10-06 01:08:46 +0900

    Add libsamplerate 0.2.2

--- gvsbuild/projects/__init__.py
+++ gvsbuild/projects/__init__.py
@@ -52,6 +52,7 @@ from gvsbuild.projects.libmicrohttpd import Libmicrohttpd
 from gvsbuild.projects.libpng import Libpng
 from gvsbuild.projects.libpsl import Libpsl
 from gvsbuild.projects.librsvg import Librsvg
+from gvsbuild.projects.libsamplerate import Libsamplerate
 from gvsbuild.projects.libsoup import Libsoup2, Libsoup3
 from gvsbuild.projects.libssh import Libssh, Libssh2
 from gvsbuild.projects.libtiff import Libtiff4
