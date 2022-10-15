bc1322fda2f052f2be4a64c41116b074c5c6a733
Stefano Sabatini <sabatin@amazon.com>
2022-10-16 00:41:44 +0200

    list_options: add list-projects --json option
    This is useful to generate output which can be easily parsed.

--- gvsbuild/list_projects.py
+++ gvsbuild/list_projects.py
@@ -14,9 +14,11 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, see <http://www.gnu.org/licenses/>.
+import json
+
 import typer
 
-from gvsbuild.utils.base_project import Project, ProjectType, get_project_by_type
+from gvsbuild.utils.base_project import Project, ProjectType
 
 
 def list_projects(
@@ -25,21 +27,43 @@ def list_projects(
         "--type",
         help="Specify type of projects to show, if not selected show all",
         rich_help_panel="Selection Options",
-    )
+    ),
+    json_: bool = typer.Option(
+        False,
+        "--json",
+        help="Show list in JSON format",
+        rich_help_panel="Formatting Options",
+    ),
 ):
     Project.add_all()
 
-    def _list_projects_by_type(project_type):
-        projects = get_project_by_type(project_type)
-        if projects:
-            projects.sort()
-
-            print(f"Available projects with type {project_type}:")
-            for project in projects:
-                print(f"\t{project[0]:<{Project.name_len}} {project[1]}")
-
+    projects = Project.list_projects()
     if project_type:
-        _list_projects_by_type(project_type)
+        projects = [
+            project for project in projects if project.type.value == project_type
+        ]
+
+    if json_:
+
+        def _get_project_data(project):
+            data = {"dependencies": project.dependencies, "type": project.type.value}
+            if project.version:
+                data["version"] = project.version
+            return data
+
+        print(
+            json.dumps(
+                {project.name: _get_project_data(project) for project in projects},
+                indent=4,
+                sort_keys=True,
+            )
+        )
     else:
         for project_type in ProjectType:
-            _list_projects_by_type(project_type)
+            type_projects = [
+                project for project in projects if project.type == project_type
+            ]
+            if type_projects:
+                print(f"Available projects with type {project_type}:")
+                for project in type_projects:
+                    print(f"\t{project.name:<{Project.name_len}} {project.version}")
