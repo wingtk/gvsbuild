75a5c649fdbe147ca336cc9f969bb4429e0c591f
Stefano Sabatini <sabatin@amazon.com>
2022-10-25 10:38:41 +0200

    tests/test_list: import and use project definitions from library

--- tests/test_list.py
+++ tests/test_list.py
@@ -15,117 +15,20 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-projects = [
-    "adwaita-icon-theme",
-    "atk",
-    "boringssl",
-    "cairo",
-    "check-libs",
-    "clutter",
-    "cogl",
-    "cyrus-sasl",
-    "dcv-color-primitives",
-    "emeus",
-    "enchant",
-    "ffmpeg",
-    "fontconfig",
-    "freerdp",
-    "freetype",
-    "fribidi",
-    "gdk-pixbuf",
-    "gettext",
-    "glib",
-    "glib-networking",
-    "glib-py-wrapper",
-    "gobject-introspection",
-    "graphene",
-    "gsettings-desktop-schemas",
-    "gst-plugins-bad",
-    "gst-plugins-base",
-    "gst-plugins-good",
-    "gst-python",
-    "gstreamer",
-    "gtk2",
-    "gtk3",
-    "gtk4",
-    "gtksourceview4",
-    "gtksourceview5",
-    "harfbuzz",
-    "hello-world",
-    "hicolor-icon-theme",
-    "icu",
-    "json-c",
-    "json-glib",
-    "leveldb",
-    "lgi",
-    "libadwaita",
-    "libarchive",
-    "libcroco",
-    "libcurl",
-    "libepoxy",
-    "libffi",
-    "libgxps",
-    "libjpeg-turbo",
-    "libmicrohttpd",
-    "libpng",
-    "libpsl",
-    "librsvg",
-    "libsoup2",
-    "libsoup3",
-    "libssh",
-    "libssh2",
-    "libtiff-4",
-    "libuv",
-    "libvpx",
-    "libxml2",
-    "libyuv",
-    "libzip",
-    "lmdb",
-    "luajit",
-    "lz4",
-    "mit-kerberos",
-    "nghttp2",
-    "nv-codec-headers",
-    "openh264",
-    "openssl",
-    "opus",
-    "orc",
-    "pango",
-    "pixman",
-    "pkg-config",
-    "portaudio",
-    "protobuf",
-    "protobuf-c",
-    "pycairo",
-    "pygobject",
-    "sqlite",
-    "win-iconv",
-    "wing",
-    "x264",
-    "zlib",
-]
-tools = [
-    "cargo",
-    "cmake",
-    "go",
-    "meson",
-    "msys2",
-    "nasm",
-    "ninja",
-    "nuget",
-    "perl",
-    "python",
-    "yasm",
-]
-groups = ["all", "gtk3-full", "tools", "tools-check"]
+import gvsbuild.groups  # noqa: F401
+import gvsbuild.projects  # noqa: F401
+import gvsbuild.tools  # noqa: F401
+from gvsbuild.utils.base_project import Project, ProjectType
 
 
 def test_list(typer_app, runner):
+    Project.add_all()
+    projects = Project.list_projects()
+
     result = runner.invoke(typer_app, ["list"])
     assert result.exit_code == 0
-    for group in groups:
-        assert group in result.stdout
-    for project in projects:
-        assert project in result.stdout
-    for tool in tools:
-        assert tool in result.stdout
+    for project_type in [ProjectType.GROUP, ProjectType.PROJECT, ProjectType.TOOL]:
+        for project_name in [
+            project.name for project in projects if project.type == project_type
+        ]:
+            assert project_name in result.stdout
