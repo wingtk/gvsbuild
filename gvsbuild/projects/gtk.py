fa8a29a493ade11a71019bfea6f3458ec53c00e0
Alessandro Bono <alessandro.bono369@gmail.com>
2022-10-31 10:14:02 +0100

    Apply Gtk3 patch for GdkWin32Selection
    See https://gitlab.gnome.org/GNOME/gtk/-/issues/5207. Even though the crash
    is reported with GLib 2.74.0, the code was definitely wrong and the fix is
    trivial. Backport the patch until the next minor release.

--- gvsbuild/projects/gtk.py
+++ gvsbuild/projects/gtk.py
@@ -93,6 +93,7 @@ class Gtk3(Tarball, Meson):
             dependencies=["atk", "gdk-pixbuf", "pango", "libepoxy"],
             patches=[
                 "gtk_update_icon_cache.patch",
+                "gdkwin32-fix-subclassing-for-gdkwin32selection.patch",
             ],
         )
         if self.opts.enable_gi:
