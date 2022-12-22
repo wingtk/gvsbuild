8268ee7b79265aba5ee36f65efbf74c5fab98bf9
Ignazio Pillai <ignazp@amazon.com>
2022-12-22 13:27:41 +0100

    gtk3: fix clipboard EOL conversion
    see https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/5366

--- gvsbuild/projects/gtk.py
+++ gvsbuild/projects/gtk.py
@@ -99,6 +99,7 @@ class Gtk3(Tarball, Meson):
             dependencies=["atk", "gdk-pixbuf", "pango", "libepoxy"],
             patches=[
                 "gtk_update_icon_cache.patch",
+                "clipboard-improve-EOL-conversion-of-plain-text.patch",
             ],
         )
         if self.opts.enable_gi:
