7e4a065487dd115fa5b7dc01c7a40c60d2041bc5
Dan Yeaw <dan@yeaw.me>
2022-12-25 08:09:03 -0500

    Update meson to version 1.0.0

--- gvsbuild/tools.py
+++ gvsbuild/tools.py
@@ -106,10 +106,10 @@ class ToolMeson(Tool):
         Tool.__init__(
             self,
             "meson",
-            version="0.64.1",
+            version="1.0.0",
             archive_url="https://github.com/mesonbuild/meson/archive/refs/tags/{version}.tar.gz",
             archive_file_name="meson-{version}.tar.gz",
-            hash="1d12a4bc1cf3ab18946d12cf0b6452e5254ada1ad50aacc97f87e2cccd7da315",
+            hash="a2ada84d43c7e57400daee80a880a1f5003d062b2cb6c9be1747b0db38f2eb8d",
             dependencies=[],
             dir_part="meson-{version}",
             exe_name="meson.py",
