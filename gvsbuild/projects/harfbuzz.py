e0c02af53bc66b4e3ef53c14a139a810c904f877
Dan Yeaw <dan@yeaw.me>
2023-03-03 20:20:15 -0500

    Update harfbuzz to version 7.1.0

--- gvsbuild/projects/harfbuzz.py
+++ gvsbuild/projects/harfbuzz.py
@@ -26,9 +26,9 @@ class Harfbuzz(Tarball, Meson):
         Project.__init__(
             self,
             "harfbuzz",
-            version="7.0.1",
+            version="7.1.0",
             archive_url="https://github.com/harfbuzz/harfbuzz/releases/download/{version}/harfbuzz-{version}.tar.xz",
-            hash="2cf4d3d8f225007511990a36a34195f0d6562ca56df0a8b0885b382837948199",
+            hash="f135a61cd464c9ed6bc9823764c188f276c3850a8dc904628de2a87966b7077b",
             dependencies=["meson", "cmake", "freetype", "pkgconf", "glib"],
         )
 
