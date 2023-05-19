44d934669ef4606f89b7b5c49921b58c2d5a9cfc
Marco Capypara Köpcke <hello@capypara.de>
2023-05-19 23:03:45 +0200

    Project gtk3.24: Include patch for crash on copy
    This includes the patch of upstream Merge Request 5690,
    which fixes a crash with GTK3 on Windows when trying to copy
    text. This has not been released in a new version yet.

--- gvsbuild/projects/gtk.py
+++ gvsbuild/projects/gtk.py
@@ -101,6 +101,7 @@ class Gtk3(Tarball, Meson):
             dependencies=["atk", "gdk-pixbuf", "pango", "libepoxy"],
             patches=[
                 "gtk_update_icon_cache.patch",
+                "gdk_fix_clipboard.patch",
             ],
         )
         if self.opts.enable_gi:
