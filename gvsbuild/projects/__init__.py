016d96d3cbff9b6bb5107304b402cbb64b691d2a
Dan Yeaw <dan@yeaw.me>
2023-05-20 09:53:46 -0400

    Add libsigc++

--- gvsbuild/projects/__init__.py
+++ gvsbuild/projects/__init__.py
@@ -57,6 +57,7 @@ from gvsbuild.projects.libmicrohttpd import Libmicrohttpd
 from gvsbuild.projects.libpng import Libpng
 from gvsbuild.projects.libpsl import Libpsl
 from gvsbuild.projects.librsvg import Librsvg
+from gvsbuild.projects.libsigcplusplus import Libsigcplusplus
 from gvsbuild.projects.libsoup import Libsoup2, Libsoup3
 from gvsbuild.projects.libssh import Libssh, Libssh2
 from gvsbuild.projects.libtiff import Libtiff4
