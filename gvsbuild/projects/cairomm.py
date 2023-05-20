4ed0459b7da495ee23546ca0162d2378104d5e40
Dan Yeaw <dan@yeaw.me>
2023-05-20 10:31:31 -0400

    Add cairomm and pangomm

---
+++ gvsbuild/projects/cairomm.py
@@ -0,0 +1,39 @@
+#  Copyright (C) 2016 - Yevgen Muntyan
+#  Copyright (C) 2016 - Ignacio Casal Quinteiro
+#  Copyright (C) 2016 - Arnavion
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from gvsbuild.utils.base_builders import Meson
+from gvsbuild.utils.base_expanders import Tarball
+from gvsbuild.utils.base_project import project_add
+
+
+@project_add
+class Cairomm(Tarball, Meson):
+    def __init__(self):
+        Meson.__init__(
+            self,
+            "cairomm",
+            version="1.16.2",
+            archive_url="https://gitlab.freedesktop.org/cairo/cairomm/-/archive/{version}/cairomm-{version}.tar.gz",
+            hash="54da2d1b51c62d9503599031979e3f132fcd6c4d507b37bd244364a0f9c692cf",
+            dependencies=["meson", "ninja", "libsigc++", "cairo"],
+        )
+
+    def build(self):
+        Meson.build(
+            self, meson_params="-Dbuild-examples=false -Dbuild-documentation=false"
+        )
+        self.install(r".\COPYING share\doc\cairomm")
---
+++ gvsbuild/projects/pangomm.py
@@ -0,0 +1,50 @@
+#  Copyright (C) 2016 - Yevgen Muntyan
+#  Copyright (C) 2016 - Ignacio Casal Quinteiro
+#  Copyright (C) 2016 - Arnavion
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from gvsbuild.utils.base_builders import Meson
+from gvsbuild.utils.base_expanders import Tarball
+from gvsbuild.utils.base_project import project_add
+
+
+@project_add
+class Pangomm(Tarball, Meson):
+    def __init__(self):
+        Meson.__init__(
+            self,
+            "pangomm",
+            prj_dir="pangomm",
+            version="2.50.1",
+            lastversion_even=True,
+            repository="https://gitlab.gnome.org/GNOME/pangomm",
+            archive_url="https://download.gnome.org/sources/pangomm/{major}.{minor}/pangomm-{version}.tar.xz",
+            hash="ccc9923413e408c2bff637df663248327d72822f11e394b423e1c5652b7d9214",
+            dependencies=[
+                "meson",
+                "ninja",
+                "libsigc++",
+                "cairomm",
+                "pango",
+            ],
+        )
+
+    def build(self):
+        Meson.build(
+            self,
+            meson_params="-Dbuild-documentation=false",
+        )
+
+        self.install(r".\COPYING share\doc\glibmm")
