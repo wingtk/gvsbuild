9a5e1575a9d57b030ae79fed922a707a64e58cf3
Dan Yeaw <dan@yeaw.me>
2023-07-08 07:55:36 -0400

    Update libpng to version 1.6.40

--- gvsbuild/projects/libpng.py
+++ gvsbuild/projects/libpng.py
@@ -26,9 +26,9 @@ class Libpng(Tarball, CmakeProject):
         Project.__init__(
             self,
             "libpng",
-            version="1.6.39",
+            version="1.6.40",
             archive_url="http://prdownloads.sourceforge.net/libpng/libpng-{version}.tar.xz",
-            hash="1f4696ce70b4ee5f85f1e1623dc1229b210029fa4b7aee573df3e2ba7b036937",
+            hash="535b479b2467ff231a3ec6d92a525906fb8ef27978be4f66dbe05d3f3a01b3a1",
             dependencies=["cmake", "ninja", "zlib"],
         )
 
