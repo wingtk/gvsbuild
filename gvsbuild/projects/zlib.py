7f782946c77a08bfc9ca429b710d087dff9fb95a
Dan Yeaw <dan@yeaw.me>
2023-08-18 13:40:37 -0400

    Update zlib to version 1.3

--- gvsbuild/projects/zlib.py
+++ gvsbuild/projects/zlib.py
@@ -38,9 +38,9 @@ class Zlib(Tarball, Project):
         Project.__init__(
             self,
             "zlib",
-            version="1.2.13",
+            version="1.3",
             archive_url="https://github.com/madler/zlib/releases/download/v{version}/zlib-{version}.tar.xz",
-            hash="d14c38e313afc35a9a8760dadf26042f51ea0f5d154b0630a31da0540107fb98",
+            hash="8a9ba2898e1d0d774eca6ba5b4627a11e5588ba85c8851336eb38de4683050a7",
         )
 
     def build(self):
