15d8f586ef6e7475c7aad8fce89aee188d10b968
Dan Yeaw <dan@yeaw.me>
2023-09-15 19:58:19 -0400

    Update sqlite to version 3.43.1

--- gvsbuild/projects/sqlite.py
+++ gvsbuild/projects/sqlite.py
@@ -23,9 +23,9 @@ class SQLite(Tarball, Project):
         Project.__init__(
             self,
             "sqlite",
-            version="3.43.0",
+            version="3.43.1",
             archive_url="https://www.sqlite.org/2023/sqlite-autoconf-{major}{minor:0<3}{micro:0<3}.tar.gz",
-            hash="49008dbf3afc04d4edc8ecfc34e4ead196973034293c997adad2f63f01762ae1",
+            hash="39116c94e76630f22d54cd82c3cea308565f1715f716d1b2527f1c9c969ba4d9",
         )
 
     def build(self):
