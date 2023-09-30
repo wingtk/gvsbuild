38b547b259c903675ecc21fe9b5c23186e20ad79
Dan Yeaw <dan@yeaw.me>
2023-09-30 15:28:01 -0400

    Add patch for glib-compile-resources not found

--- gvsbuild/tools.py
+++ gvsbuild/tools.py
@@ -99,6 +99,7 @@ class ToolMeson(Tool):
             hash="1caa0ef6082e311bdca9836e7907f548b8c3f041a42ed41f0ff916b83ac7dddd",
             dir_part="meson-{version}",
             exe_name="meson.py",
+            patches=["0001-find-tool-pkgconfig-variable.patch"],
         )
 
     def unpack(self):
