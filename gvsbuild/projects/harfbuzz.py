27d8a612d391cd28c0f45bf3200b86eee1fd1ebc
Dan Yeaw <dan@yeaw.me>
2023-10-27 20:20:17 -0400

    Update HarfBuzz to version 8.2.2

--- gvsbuild/projects/harfbuzz.py
+++ gvsbuild/projects/harfbuzz.py
@@ -24,9 +24,9 @@ class Harfbuzz(Tarball, Meson):
         Project.__init__(
             self,
             "harfbuzz",
-            version="8.2.1",
+            version="8.2.2",
             archive_url="https://github.com/harfbuzz/harfbuzz/releases/download/{version}/harfbuzz-{version}.tar.xz",
-            hash="0fec78f98c9c8faf228957a201c8846f809452c20f8445eb092a1ba6f22dbea5",
+            hash="e433ad85fbdf57f680be29479b3f964577379aaf319f557eb76569f0ecbc90f3",
             dependencies=["meson", "cmake", "freetype", "cairo", "pkgconf", "glib"],
         )
 
