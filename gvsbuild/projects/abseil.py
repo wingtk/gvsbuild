f42b8559828a0978edae62b576f9159f6f7b4325
Alessandro Bono <alessandro.bono369@gmail.com>
2023-12-13 11:03:29 +0100

    Add AbseilCpp project
    Closes: https://github.com/wingtk/gvsbuild/issues/1174

---
+++ gvsbuild/projects/abseil.py
@@ -0,0 +1,42 @@
+#  Copyright (C) 2024 The Gvsbuild Authors
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from gvsbuild.utils.base_builders import CmakeProject
+from gvsbuild.utils.base_expanders import Tarball
+from gvsbuild.utils.base_project import Project, project_add
+
+
+@project_add
+class AbseilCpp(Tarball, CmakeProject):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "abseil-cpp",
+            version="20230802.1",
+            archive_url="https://github.com/abseil/abseil-cpp/archive/refs/tags/{version}.tar.gz",
+            hash="987ce98f02eefbaf930d6e38ab16aa05737234d7afbab2d5c4ea7adbe50c28ed",
+            dependencies=[
+                "cmake",
+                "ninja",
+                "zlib",
+            ],
+        )
+
+    def build(self):
+        CmakeProject.build(
+            self,
+            cmake_params=r"-DBUILD_SHARED_LIBS=ON",
+            use_ninja=True,
+        )
