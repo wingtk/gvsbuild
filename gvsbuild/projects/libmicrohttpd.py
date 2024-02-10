a80f35e4c047c9e9d7100778d57c3628f2b13deb
Dan Yeaw <dan@yeaw.me>
2024-02-10 09:49:39 -0500

    Update patch for libmicrohttpd

--- gvsbuild/projects/libmicrohttpd.py
+++ gvsbuild/projects/libmicrohttpd.py
@@ -29,7 +29,7 @@ class Libmicrohttpd(Tarball, Project):
             repository="https://github.com/Karlson2k/libmicrohttpd",
             archive_url="http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-{version}.tar.gz",
             hash="a02792d3cd1520e2ecfed9df642079d44a36ed87167442b28d7ed19e906e3e96",
-            patches=["001-remove-postsample.patch"],
+            patches=["001-remove-postsample-perf-retries.patch"],
         )
 
     def build(self):
