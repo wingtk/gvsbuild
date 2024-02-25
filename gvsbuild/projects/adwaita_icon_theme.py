6ecaa36b02432b0e991ebd550b86a4218a18af5b
Dan Yeaw <dan@yeaw.me>
2024-02-25 11:27:42 -0500

    Fix incorrect filename syntax for symlink

--- gvsbuild/projects/adwaita_icon_theme.py
+++ gvsbuild/projects/adwaita_icon_theme.py
@@ -13,6 +13,8 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+import os
+
 from gvsbuild.utils.base_builders import Meson
 from gvsbuild.utils.base_expanders import Tarball
 from gvsbuild.utils.base_project import project_add
@@ -37,4 +39,16 @@ class AdwaitaIconTheme(Tarball, Meson):
 
     def build(self):
         Meson.build(self)
+        # Work around for https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/issues/282
+        self.builder.exec_msys(
+            [
+                "cp",
+                "--remove-destination",
+                "places/folder.svg",
+                "status/folder-open.svg",
+            ],
+            working_dir=os.path.join(
+                self.builder.gtk_dir, "share", "icons", "Adwaita", "scalable"
+            ),
+        )
         self.install(r".\COPYING_CCBYSA3 share\doc\adwaita-icon-theme")
