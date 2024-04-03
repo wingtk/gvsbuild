ad74480ef48d441af8806ef365d9f7b1714a2cf7
Marco Köpcke <marco.koepcke@tudock.de>
2024-04-03 19:53:15 +0200

    Update libadwaita to version 1.5.0

--- gvsbuild/projects/libadwaita.py
+++ gvsbuild/projects/libadwaita.py
@@ -25,10 +25,10 @@ class Libadwaita(Tarball, Meson):
             self,
             "libadwaita",
             repository="https://gitlab.gnome.org/GNOME/libadwaita",
-            version="1.4.4",
+            version="1.5.0",
             lastversion_even=True,
             archive_url="https://download.gnome.org/sources/libadwaita/{major}.{minor}/libadwaita-{version}.tar.xz",
-            hash="f802b7d8d5ae33be4650ef571a580f144a806202a26f527dacd57d1560938828",
+            hash="fd92287df9bb95c963654fb6e70d3e082e2bcb37b147e0e3c905567167993783",
             dependencies=[
                 "ninja",
                 "meson",
