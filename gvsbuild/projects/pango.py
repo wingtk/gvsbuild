09d76401a2e03130b42afebb9587f46626ddc518
Dan Yeaw <dan@yeaw.me>
2024-11-30 12:15:46 -0500

    Update Pango to version 1.55.0

--- gvsbuild/projects/pango.py
+++ gvsbuild/projects/pango.py
@@ -24,10 +24,10 @@ class Pango(Tarball, Meson):
         Project.__init__(
             self,
             "pango",
-            version="1.54.0",
+            version="1.55.0",
             repository="https://gitlab.gnome.org/GNOME/pango",
             archive_url="https://download.gnome.org/sources/pango/{major}.{minor}/pango-{version}.tar.xz",
-            hash="8a9eed75021ee734d7fc0fdf3a65c3bba51dfefe4ae51a9b414a60c70b2d1ed8",
+            hash="a2c17a8dc459a7267b8b167bb149d23ff473b6ff9d5972bee047807ee2220ccf",
             dependencies=[
                 "ninja",
                 "meson",
