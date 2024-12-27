aacb5aa94a3c89618a93759474e5587b2f4252ab
Dan Yeaw <dan@yeaw.me>
2024-12-27 14:22:09 -0500

    Add libnice for building gstwebrtc

---
+++ gvsbuild/projects/libnice.py
@@ -0,0 +1,39 @@
+#  Copyright (C) 2024 The Gvsbuild Authors
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from gvsbuild.utils.base_builders import Meson
+from gvsbuild.utils.base_expanders import GitRepo
+from gvsbuild.utils.base_project import Project, project_add
+
+
+@project_add
+class Libnice(GitRepo, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "libnice",
+            version="0.1.22",
+            repo_url="https://gitlab.freedesktop.org/libnice/libnice.git",
+            fetch_submodules=False,
+            tag="ae3eb16fd7d1237353bf64e899c612b8a63bca8a",
+            dependencies=[
+                "ninja",
+                "meson",
+            ],
+        )
+
+    def build(self):
+        Meson.build(self)
+        self.install(r"LICENSE share\doc\libnice")
