39b5db68f0f97e96548e1d47390b8715116c5b4a
Dan Yeaw <dan@yeaw.me>
2025-03-09 20:21:09 -0400

    Update pkgconf to version 2.4.3

--- gvsbuild/projects/pkgconf.py
+++ gvsbuild/projects/pkgconf.py
@@ -25,9 +25,9 @@ class PkgConf(Tarball, Meson):
             self,
             "pkgconf",
             prj_dir="pkgconf",
-            version="2.4.1",
+            version="2.4.3",
             archive_url="https://distfiles.ariadne.space/pkgconf/pkgconf-{version}.tar.gz",
-            hash="b895456387f2f5058cbfe63ed69f924b06701a13f0c9bef63fcc576d33011346",
+            hash="cf6be37c79265802f2cb1dfc412e18de23a35b5204fc5868bc09fcfd092ac225",
             dependencies=["ninja", "meson"],
             patches=["0001-libpkgconf-add-defines-to-unbreak-build-with-VS2013.patch"],
         )
