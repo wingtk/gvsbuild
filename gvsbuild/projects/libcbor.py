5a47e8c02969d1a1a612152758b18ae78931692f
Dan Yeaw <dan@yeaw.me>
2025-03-18 22:00:03 -0400

    Update libcbor to version 0.12.0

--- gvsbuild/projects/libcbor.py
+++ gvsbuild/projects/libcbor.py
@@ -24,10 +24,10 @@ class Libcbor(Tarball, CmakeProject):
         Project.__init__(
             self,
             "libcbor",
-            version="0.11.0",
+            version="0.12.0",
             archive_url="https://github.com/PJK/libcbor/archive/refs/tags/v{version}.tar.gz",
             archive_filename="libcbor-{version}.tar.gz",
-            hash="89e0a83d16993ce50651a7501355453f5250e8729dfc8d4a251a78ea23bb26d7",
+            hash="5368add109db559f546d7ed10f440f39a273b073daa8da4abffc83815069fa7f",
             dependencies=["cmake", "ninja"],
         )
 
