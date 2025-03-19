2128d10a9fbe35602e2f5b0c064cd26acb39037b
Dan Yeaw <dan@yeaw.me>
2025-03-18 22:28:09 -0400

    Update Cargo to version 1.85.1

--- gvsbuild/tools.py
+++ gvsbuild/tools.py
@@ -28,7 +28,7 @@ class ToolCargo(Tool):
         Tool.__init__(
             self,
             "cargo",
-            version="1.85.0",
+            version="1.85.1",
             repository="rust-lang/rust",
             archive_url="https://win.rustup.rs/x86_64",
             archive_filename="rustup-init.exe",
