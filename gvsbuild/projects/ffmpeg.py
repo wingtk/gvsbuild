7477b8101b78da9f2aeb699300c9be03272252b4
Dan Yeaw <dan@yeaw.me>
2025-08-30 10:40:10 -0400

    Update FFmpeg to version 8.0

--- gvsbuild/projects/ffmpeg.py
+++ gvsbuild/projects/ffmpeg.py
@@ -26,9 +26,9 @@ class Ffmpeg(Tarball, Project):
         Project.__init__(
             self,
             "ffmpeg",
-            version="7.1.1",
+            version="8.0",
             archive_url="https://ffmpeg.org/releases/ffmpeg-{version}.tar.xz",
-            hash="733984395e0dbbe5c046abda2dc49a5544e7e0e1e2366bba849222ae9e3a03b1",
+            hash="b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e",
             dependencies=[
                 "dav1d",
                 "nasm",
