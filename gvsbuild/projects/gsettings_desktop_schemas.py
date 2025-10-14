9067eb06bd8027e2b3917b481118c247ba45b885
Dan Yeaw <dan@yeaw.me>
2025-10-13 22:03:51 -0400

    Update gsettings-desktop-schemas to version 49.1

--- gvsbuild/projects/gsettings_desktop_schemas.py
+++ gvsbuild/projects/gsettings_desktop_schemas.py
@@ -24,10 +24,10 @@ class GSettingsDesktopSchemas(Tarball, Meson):
         Project.__init__(
             self,
             "gsettings-desktop-schemas",
-            version="49.0",
+            version="49.1",
             repository="https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas",
             archive_url="https://download.gnome.org/sources/gsettings-desktop-schemas/{major}/gsettings-desktop-schemas-{version}.tar.xz",
-            hash="912905cc45382888a47702ed1101c6b08ebd0122a32a67d940ab8116a96c520d",
+            hash="777a7f83d5e5a8076b9bf809cb24101b1b1ba9c230235e3c3de8e13968ed0e63",
             dependencies=["meson", "ninja", "pkgconf", "glib"],
         )
         if self.opts.enable_gi:
