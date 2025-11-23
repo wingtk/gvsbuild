30db9bcf5167595b6ba8cec99043ce84c09d89bc
Dan Yeaw <dan@yeaw.me>
2025-11-23 08:32:40 -0500

    Rename static libraries for GTK

--- gvsbuild/projects/directx_headers.py
+++ gvsbuild/projects/directx_headers.py
@@ -26,6 +26,8 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+import os
+
 from gvsbuild.utils.base_builders import Meson
 from gvsbuild.utils.base_expanders import Tarball
 from gvsbuild.utils.base_project import Project, project_add
@@ -49,3 +51,14 @@ class DirectXHeaders(Tarball, Meson):
     def build(self):
         Meson.build(self, meson_params="-Dbuild-test=false")
         self.install(r".\LICENSE share\doc\directx-headers")
+
+    def post_install(self):
+        lib_dir = os.path.join(self.builder.gtk_dir, "lib")
+        self.builder.exec_msys(
+            ["mv", "libd3dx12-format-properties.a", "d3dx12-format-properties.lib"],
+            working_dir=lib_dir,
+        )
+        self.builder.exec_msys(
+            ["mv", "libDirectX-Guids.a", "DirectX-Guids.lib"],
+            working_dir=lib_dir,
+        )
