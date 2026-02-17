eec46f8b02261990c739b41668742237bc019b21
Dan Yeaw <dan@yeaw.me>
2026-02-17 10:02:53 -0500

    Add ministream project

---
+++ gvsbuild/projects/ministream.py
@@ -0,0 +1,40 @@
+#  Copyright (C) 2016 The Gvsbuild Authors
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from gvsbuild.utils.base_builders import Meson
+from gvsbuild.utils.base_expanders import GitRepo
+from gvsbuild.utils.base_project import Project, project_add
+
+
+@project_add
+class Ministream(GitRepo, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "ministream",
+            repository="https://gitlab.gnome.org/sp1rit/ministream",
+            fetch_submodules=False,
+            tag="main",
+            dependencies=["meson", "glib"],
+        )
+        if self.opts.enable_gi:
+            self.add_dependency("gobject-introspection")
+        enable_gi = "enabled" if self.opts.enable_gi else "disabled"
+        self.add_param(f"-Dintrospection={enable_gi}")
+
+    def build(self):
+        Meson.build(self)
+        # Need a license
+        # self.install(r".\COPYING share\doc\ministream")
