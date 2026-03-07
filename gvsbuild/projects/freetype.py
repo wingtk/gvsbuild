696b31da6d9f661ace3f5a6ddd18bf37d61c8d37
Dan Yeaw <dan@yeaw.me>
2026-03-07 15:47:20 -0500

    Update freetype to version 2.14.2

--- gvsbuild/projects/freetype.py
+++ gvsbuild/projects/freetype.py
@@ -24,10 +24,10 @@ class Freetype(Tarball, Meson):
         Project.__init__(
             self,
             "freetype",
-            version="2.14.1",
+            version="2.14.2",
             repository="https://gitlab.freedesktop.org/freetype/freetype",
             archive_url="https://downloads.sourceforge.net/project/freetype/freetype2/{version}/freetype-{version}.tar.xz",
-            hash="32427e8c471ac095853212a37aef816c60b42052d4d9e48230bab3bdf2936ccc",
+            hash="4b62dcab4c920a1a860369933221814362e699e26f55792516d671e6ff55b5e1",
             dependencies=["pkgconf", "ninja", "libpng"],
             patches=["0001-meson-in-shared-libraries-we-need-to-export-the-meth.patch"],
         )
