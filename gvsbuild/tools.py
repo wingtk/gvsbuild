bff7d52f8967d537eff40413f4a6420edac259e2
Dan Yeaw <dan@yeaw.me>
2026-08-01 21:09:41 -0400

    Fix Go hash

--- gvsbuild/tools.py
+++ gvsbuild/tools.py
@@ -225,7 +225,7 @@ class ToolGo(Tool):
             version="1.26.5",
             repository="https://github.com/golang/go",
             archive_url="https://go.dev/dl/go{version}.windows-amd64.zip",
-            hash="3ca8fb4630b07c419cbdd51f754e31363cfcfb83b3a5354d9e895c90be2cc345",
+            hash="97e6b2a833b6d89f9ff17d25419ac0a7e3b482a044e9ab18cdef834bd834fd38",
             dir_part="go-{version}",
         )
 
