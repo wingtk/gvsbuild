912e3cefce5a71269d1c31bda8837935b47fbdb1
Dan Yeaw <dan@yeaw.me>
2026-08-01 20:47:20 -0400

    Update nghttp2 to version 1.70.0

--- gvsbuild/projects/nghttp2.py
+++ gvsbuild/projects/nghttp2.py
@@ -24,10 +24,10 @@ class Nghttp2(Tarball, CmakeProject):
         Project.__init__(
             self,
             "nghttp2",
-            version="1.69.0",
+            version="1.70.0",
             repository="https://github.com/nghttp2/nghttp2",
             archive_url="https://github.com/nghttp2/nghttp2/releases/download/v{version}/nghttp2-{version}.tar.xz",
-            hash="1fb324b6ec2c56f6bde0658f4139ffd8209fa9e77ce98fd7a5f63af8d0e508ad",
+            hash="e05cb1388eaca3830aded4ccf20044b6e1ac1a61411dcca11b0437c4285c8bc2",
             dependencies=[
                 "cmake",
                 "zlib",
