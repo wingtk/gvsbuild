ce1519cb830d148ff0357fcf5b828c8a3e9b11f8
Zhang Wen <zhw2101024@gmail.com>
2026-08-16 02:44:06 +0800

    clear RUSTC_WRAPPER when env not empty

--- gvsbuild/projects/gperf.py
+++ gvsbuild/projects/gperf.py
@@ -26,7 +26,7 @@ class Gperf(Tarball, Meson):
             "gperf",
             version="3.1",
             repository="https://savannah.gnu.org/git/?group=gperf",
-            archive_url="https://mirrors.ibiblio.org/gnu/gperf/gperf-{version}.tar.gz",
+            archive_url="https://ftpmirror.gnu.org/gnu/gperf/gperf-{version}.tar.gz",
             hash="588546b945bba4b70b6a3a616e80b4ab466e3f33024a352fc2198112cdbb3ae2",
             dependencies=["ninja"],
         )
